56d985208a0cf91b8ab6c71a381a7c7037842f32
eng-mohamed-eldeeb <agnabiieldeeeb@gmail.com>
2023-11-21 19:07:35 +0200

    feat: add user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
-  root "users#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  devise_for :users, defaults: { format: :json }
   # Defines the root path route ("/")
   # root "articles#index"
 end
