64e63c7d6cf426a87d1390d137a0d974804a2ecc
eng-mohamed-eldeeb <agnabiieldeeeb@gmail.com>
2023-12-17 18:23:29 +0200

    feat: reset the password and swagger

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'application#confirmed'
-  devise_for :users, controllers: { confirmations: 'users/confirmations' }, defaults: { format: :json }
-  # Defines the root path route ("/")
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords'  # Specify the custom controller for passwords
+  }
+    # Defines the root path route ("/")
   # root "articles#index"
 end
