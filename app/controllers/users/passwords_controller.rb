52640a1ce51167f5ec64c8e34adc5850571692cd
eng-mohamed-eldeeb <agnabiieldeeeb@gmail.com>
2023-12-25 21:45:47 +0200

    fix: the password 500 error

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -8,9 +8,15 @@ class Users::PasswordsController < Devise::PasswordsController
   # end
 
   # POST /resource/password
-  # def create
-  #   super
-  # end
+  def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    yield resource if block_given?
+    if successfully_sent?(resource)
+      return render json: { message: "#{t 'password_reset.success'}" }
+    else
+      respond_with(resource)
+    end
+  end
 
   # GET /resource/password/edit?reset_password_token=abcdef
   def edit
